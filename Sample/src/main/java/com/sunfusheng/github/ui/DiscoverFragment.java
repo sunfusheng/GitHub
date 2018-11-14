f3f1b53aff0ee635b554d9ae5d0302f7a14e1ec2
sunfusheng <sfsheng0322@gmail.com>
2018-11-14 20:26:17 +0800

    升级配置

--- Sample/src/main/java/com/sunfusheng/github/ui/DiscoverFragment.java
+++ Sample/src/main/java/com/sunfusheng/github/ui/DiscoverFragment.java
@@ -1,19 +1,11 @@
 package com.sunfusheng.github.ui;
 
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.Layout;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.sunfusheng.github.R;
 import com.sunfusheng.multistate.LoadingState;
