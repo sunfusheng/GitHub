635dcbd47d4e1f65f23fb55530d40d78228f602c
sunfusheng <sfsheng0322@gmail.com>
2020-01-12 22:23:00 +0800

    将发现页的viewpager换成viewpager2

--- app/src/main/java/com/sunfusheng/github/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/sunfusheng/github/ui/discover/DiscoverFragment.java
@@ -1,25 +1,30 @@
 package com.sunfusheng.github.ui.discover;
 
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.sunfusheng.github.R;
 import com.sunfusheng.github.ui.base.BaseFragment;
-import com.sunfusheng.github.widget.bottombar.FragmentPagerItemAdapter;
+import com.sunfusheng.github.ui.base.FragmentViewPager2Adapter;
 
 /**
  * @author sunfusheng on 2018/4/18.
  */
 public class DiscoverFragment extends BaseFragment {
+    private TabLayout vTabLayout;
+    private ViewPager2 vViewPager2;
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-
+    private static final int DAILY = 0;
+    private static final int WEEKLY = 1;
+    private static final int MONTHLY = 2;
     private String[] TAB_NAMES = new String[]{"Daily", "Weekly", "Monthly"};
 
     @Override
@@ -34,8 +39,8 @@ public class DiscoverFragment extends BaseFragment {
 
     @Override
     public void initView(@NonNull View rootView) {
-        tabLayout = rootView.findViewById(R.id.tabLayout);
-        viewPager = rootView.findViewById(R.id.viewPager);
+        vTabLayout = rootView.findViewById(R.id.tabLayout);
+        vViewPager2 = rootView.findViewById(R.id.viewPager2);
 
         initViewPager();
     }
@@ -43,22 +48,24 @@ public class DiscoverFragment extends BaseFragment {
     @Override
     protected void initToolBar() {
         super.initToolBar();
-        toolbar.setTitle("Tending");
+        toolbar.setTitle(R.string.tab_discover);
     }
 
     private void initViewPager() {
-        tabLayout.addTab(tabLayout.newTab().setText(TAB_NAMES[0]), true);
-        tabLayout.addTab(tabLayout.newTab().setText(TAB_NAMES[1]), false);
-        tabLayout.addTab(tabLayout.newTab().setText(TAB_NAMES[2]), false);
+        vTabLayout.addTab(vTabLayout.newTab().setText(TAB_NAMES[DAILY]), true);
+        vTabLayout.addTab(vTabLayout.newTab().setText(TAB_NAMES[WEEKLY]), false);
+        vTabLayout.addTab(vTabLayout.newTab().setText(TAB_NAMES[MONTHLY]), false);
 
-        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter.Builder(getContext(), getChildFragmentManager())
-                .add(TAB_NAMES[0], TendingRepoListFragment.newFragment("daily"))
-                .add(TAB_NAMES[1], TendingRepoListFragment.newFragment("weekly"))
-                .add(TAB_NAMES[2], TendingRepoListFragment.newFragment("monthly"))
-                .build();
-        viewPager.setAdapter(adapter);
-        viewPager.setOffscreenPageLimit(1);
-        viewPager.setCurrentItem(0);
-        tabLayout.setupWithViewPager(viewPager);
+        SparseArray<Fragment> fragments = new SparseArray<>();
+        fragments.put(DAILY, TendingRepoListFragment.newFragment("daily"));
+        fragments.put(WEEKLY, TendingRepoListFragment.newFragment("weekly"));
+        fragments.put(MONTHLY, TendingRepoListFragment.newFragment("monthly"));
+
+        FragmentViewPager2Adapter viewPager2Adapter = new FragmentViewPager2Adapter(this);
+        viewPager2Adapter.setFragments(fragments);
+        vViewPager2.setAdapter(viewPager2Adapter);
+        new TabLayoutMediator(vTabLayout, vViewPager2, (tab, position) -> {
+            tab.setText(TAB_NAMES[position]);
+        }).attach();
     }
 }
